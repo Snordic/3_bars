9b16a43d55af5b01f8c658dbafbcf425b2b4ef7f
Snordic <artstr16@gmail.com>
2018-03-19 20:04:53 +0500

    Исключения при работе с JSON файлом

--- bars.py
+++ bars.py
@@ -5,8 +5,8 @@ import sys
 
 def load_data(filepath):
     with open(filepath, 'r', encoding='utf-8') as file_with_json:
-        data_json = json.load(file_with_json)
-    return data_json['features']
+        data_from_file = json.load(file_with_json)
+    return data_from_file['features']
 
 
 def get_biggest_bar(data_json):
@@ -24,19 +24,24 @@ def get_smallest_bar(data_json):
 def get_closest_bar(data_json, longitude, latitude):
     closest_bar = min(data_json,
                       key=lambda x: sqrt(
-                          (longitude - x['geometry']['coordinates'][0])**2 +
-                          (latitude - x['geometry']['coordinates'][1])**2)
+                          (longitude-x['geometry']['coordinates'][0])**2 +
+                          (latitude-x['geometry']['coordinates'][1])**2)
                       )
     return closest_bar['properties']['Attributes']['Name']
 
 
 if __name__ == '__main__':
-    filename = sys.argv[1]
-    data_json = load_data(filename)
-    print('Введите ваши координаты x,y= ')
-    coordinate_x, coordinate_y = input().split(',')
-    data_json = load_data(filename)
-    print(' Самый большой бар: ' + get_biggest_bar(data_json) +
-          ';\n Самый маленький бар: ' + get_smallest_bar(data_json) +
-          ';\n Самый близкий бар: ' +
-          get_closest_bar(data_json, float(coordinate_x), float(coordinate_y)))
+    try:
+        filename = sys.argv[1]
+        data_json = load_data(filename)
+    except IndexError: print('Ошибка: Вы не добавили файл!')
+    except TypeError: print('Ошибка: Вы не добавили файл!')
+    except json.decoder.JSONDecodeError: print('Ошибка: Расширение файла не JSON!')
+    else:
+        print('Введите ваши координаты x,y= ')
+        coordinate_x, coordinate_y = input().split(',')
+        data_json = load_data(filename)
+        print(' Самый большой бар: ' + get_biggest_bar(data_json) +
+              ';\n Самый маленький бар: ' + get_smallest_bar(data_json) +
+              ';\n Самый близкий бар: ' +
+              get_closest_bar(data_json, float(coordinate_x), float(coordinate_y)))
