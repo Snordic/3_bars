6422de47e16649f8349a3fd0018f32e22a1627f9
Snordic <artstr16@gmail.com>
2018-03-30 19:08:08 +0500

    Переписал функций

--- bars.py
+++ bars.py
@@ -1,19 +1,6 @@
 import json
 from math import sqrt
 import sys
-import operator
-
-
-def input_user_coordinate():
-    print('Введите ваши координаты x,y= ')
-    try:
-        x, y = input().split(',')
-        coordinate_x, coordinate_y = float(x), float(y)
-    except ValueError:
-        print('Ошибка: Координаты должны быть числами и вводится через запятую!')
-        return None, None
-    else:
-        return coordinate_x, coordinate_y
 
 
 def load_data(filepath):
@@ -25,50 +12,39 @@ def take_seat_count_bar(data_bar):
     return data_bar['properties']['Attributes']['SeatsCount']
 
 
-def get_biggest_bar(information_bar):
-    return max(information_bar,key=lambda x: (take_seat_count_bar(x)))
+def get_smallest_bar(data_from_file):
+    return min(data_from_file, key=lambda x: take_seat_count_bar(x))
 
 
-def get_smallest_bar(information_bar):
-    return min(information_bar, key=lambda x: (take_seat_count_bar(x)))
+def get_biggest_bar(data_from_file):
+    return max(data_from_file, key=lambda x: take_seat_count_bar(x))
 
 
-def get_closest_bar(information_bar):
-    longitude, latitude = input_user_coordinate()
-    if longitude and latitude:
-        return min(information_bar, key=lambda x: (calculate_distance(
-            longitude,
-            latitude,
-            *x['geometry']['coordinates']))
-                   )
-    else:
-        return None
+def get_closest_bar(data_from_file, longitude, latitude):
+    return min(data_from_file,
+               key=lambda x: (calculate_distance(
+                   longitude,
+                   latitude,
+                   *x['geometry']['coordinates']))
+               )
 
 
 def calculate_distance(x_coord_1, y_coord_1, x_coord_2, y_coord_2):
     return sqrt((x_coord_1-x_coord_2)**2 + (y_coord_1-y_coord_2)**2)
 
 
-def bar_name(data_bar):
-    if data_bar:
-        return data_bar['properties']['Attributes']['Name']
-    else:
+def get_user_coordinate():
+    print('Введите ваши координаты x,y= ')
+    try:
+        x, y = input().split(',')
+        coordinate_x, coordinate_y = float(x), float(y)
+        return coordinate_x, coordinate_y
+    except ValueError:
         return None
 
 
-def get_names_bars(info_bars):
-    name_biggest_bar = bar_name(get_biggest_bar(info_bars))
-    name_smallest_bar = bar_name(get_smallest_bar(info_bars))
-    name_closest_bar = bar_name(get_closest_bar(info_bars))
-    if not name_closest_bar:
-        name_closest_bar = 'Бар не найден из-за ошибки ввода координат!'
-    return name_biggest_bar, name_smallest_bar, name_closest_bar
-
-
-def print_search_bar(biggest_bar, smallest_bar, closest_bar):
-    print('{} {}'.format('Самый большой бар:', biggest_bar))
-    print('{} {}'.format('Самый маленький бар:', smallest_bar))
-    print('{} {}'.format('Самый близкий к вам бар:', closest_bar))
+def print_bar_name(bar_discript, bar):
+    print('{} {}'.format(bar_discript, bar['properties']['Attributes']['Name']))
 
 
 if __name__ == '__main__':
@@ -83,4 +59,14 @@ if __name__ == '__main__':
     except json.decoder.JSONDecodeError:
         print('Ошибка: Расширение файла не JSON!')
     else:
-        print_search_bar(*get_names_bars(info_bars))
+        user_coordinate = get_user_coordinate()
+        print_bar_name('Самый большой бар:',
+                       get_biggest_bar(info_bars))
+        print_bar_name('Самый маленький бар:',
+                       get_smallest_bar(info_bars))
+        if user_coordinate:
+            print_bar_name('Самый близкий бар:',
+                           get_closest_bar(info_bars, *user_coordinate))
+        else:
+            print('Самый близкий бар не был найден из-за'
+                  'ошибки ввода координат')
